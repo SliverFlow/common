b74a3999e58584d13501c174054f3f34d88a19c6
zhengjunxing <202606540@qq.com>
2024-06-29 21:50:48 +0800

    update

---
+++ middleware/tacker.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/otel"
+)
+
+type Tacker struct {
+}
+
+func NewTacker() *Tacker {
+	return &Tacker{}
+}
+
+func (t *Tacker) Handle() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tracer := otel.Tracer("ParentTracer")
+		ctx, span := tracer.Start(c.Request.Context(), c.Request.URL.Path)
+		defer span.End()
+
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
